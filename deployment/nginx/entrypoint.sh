724955de208251f3b2b154ec20ac62215965ea19
Pandu Septian Gumilar <pandusg404@gmail.com>
2023-11-10 14:51:47 +0700

    feat: portal proxy

--- deployment/nginx/entrypoint.sh
+++ deployment/nginx/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Generate a self-signed certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/server.key -out /etc/nginx/certs/server.crt -subj "/C=ID/O=Xignature/CN=localhost"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/server.key -out /etc/nginx/certs/server.crt -subj "/C=ID/O=Xignature/CN=${HOST_URL}"
 
 # Replace NGINX_URL variable in nginx.conf with the actual hostname
-sed -i "s/\${NGINX_URL}/$NGINX_URL/g" /etc/nginx/conf.d/default.conf
+sed -i "s/\${HOST_URL}/$HOST_URL/g" /etc/nginx/conf.d/default.conf
 
 # Start Nginx
 exec nginx -g 'daemon off;'
