a816801531a107f3464113d473dd98d29e874525
Pandu Septian Gumilar <pandusg404@gmail.com>
2023-11-28 15:20:49 +0700

    feat: new version

--- deployment/nginx/entrypoint.sh
+++ deployment/nginx/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Generate a self-signed certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/server.key -out /etc/nginx/certs/server.crt -subj "/C=ID/O=Xignature/CN=${HOST_URL}"
+if [ ! -f "/etc/nginx/certs/server.crt" ] || [ ! -f "/etc/nginx/certs/server.key" ]; then
+    # Generate a self-signed certificate
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/server.key -out /etc/nginx/certs/server.crt -subj "/C=ID/O=Xignature/CN=${HOST_URL}"
+fi
 
 # Replace NGINX_URL variable in nginx.conf with the actual hostname
 sed -i "s/\${HOST_URL}/$HOST_URL/g" /etc/nginx/conf.d/default.conf
